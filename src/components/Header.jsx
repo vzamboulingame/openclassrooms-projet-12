4744c2fbf2fe56ca43f080ab1e799beaaa187fee
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-06-15 11:30:16 +0200

    Finished Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,5 +7,25 @@ import logo from "../images/header-logo.svg";
  * @returns {JSX.Element} - Rendered component.
  */
 export default function Header() {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <a className="header-nav-logo" href="/">
+          <img src={logo} alt="Sportsee Logo" />
+        </a>
+        <a className="header-nav-link" href="#">
+          Accueil
+        </a>
+        <a className="header-nav-link" href="#">
+          Profil
+        </a>
+        <a className="header-nav-link" href="#">
+          Réglage
+        </a>
+        <a className="header-nav-link header-nav-link-last" href="#">
+          Communauté
+        </a>
+      </nav>
+    </header>
+  );
 }
