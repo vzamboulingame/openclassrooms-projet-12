e19fbdcd82e038c1f2b6386bdaf164c6dccc720a
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-06-15 10:29:07 +0200

    Made various changes :
    - Added Header.jsx basic file
    - Added header logo file
    - Added scss file for Header component
    - Added local Roboto fonts
    - Added fonts reference in scss files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Header from "./components/Header";
 import "./styles/style.scss";
 
+/**
+ * Main App component.
+ *
+ * @param {void}
+ * @returns {JSX.Element} Rendered component.
+ */
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../images/header-logo.svg";
+
+/**
+ * Header component.
+ *
+ * @param {void}
+ * @returns {JSX.Element} - Rendered component.
+ */
+export default function Header() {
+  return <header className="header"></header>;
+}
