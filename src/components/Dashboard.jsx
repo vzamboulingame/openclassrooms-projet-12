9deb4224669b8000329f266177a172257ea0e8f0
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-06-19 17:30:37 +0200

    Made various changes :
    - Added grid structure for dashboard charts
    - Added header greeting and message
    - Added styling for all Dashboard placeholders
    - Changed tablet brakpoint to fix display on 1024*768 resolution

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,41 @@
 export default function Dashboard() {
   return (
     <section className="dashboard">
-      <h1 className="dashboard-header">Dashboard</h1>
+      <div className="dashboard-header">
+        <h1 className="dashboard-header-greeting">
+          Bonjour <span className="dashboard-header-name">Thomas</span>
+        </h1>
+        <p className="dashboard-header-message">
+          Félicitations ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </div>
+
+      <div className="dashboard-charts">
+        <div className="dashboard-charts-activity dashboard-charts-all">
+          Activity
+        </div>
+        <div className="dashboard-charts-sessions dashboard-charts-all">
+          Average Sessions
+        </div>
+        <div className="dashboard-charts-performance dashboard-charts-all">
+          Performance
+        </div>
+        <div className="dashboard-charts-objective dashboard-charts-all">
+          Objective
+        </div>
+        <div className="dashboard-charts-calories dashboard-charts-all">
+          Calories
+        </div>
+        <div className="dashboard-charts-proteines dashboard-charts-all">
+          Proteines
+        </div>
+        <div className="dashboard-charts-glucides dashboard-charts-all">
+          Glucides
+        </div>
+        <div className="dashboard-charts-lipides dashboard-charts-all">
+          Lipides
+        </div>
+      </div>
     </section>
   );
 }
