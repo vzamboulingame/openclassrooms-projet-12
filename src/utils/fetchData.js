84d04fccd8b8c91e700d2e86c1440f5b6faee7c6
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-06-27 10:39:30 +0200

    Added jsdoc to fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,10 @@
+/**
+ * Fetches data from the specified URL.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Object} - An object with the fetched data.
+ * @throws {Error} - If an error occurs during the fetching process.
+ */
 export async function fetchData(url) {
   try {
     const response = await fetch(url);
