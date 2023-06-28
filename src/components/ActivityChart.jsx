89eef9397e37484164c7b208b7dfdf970511ad37
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-06-28 16:59:28 +0200

    Made changes to ActivityChart :
    - Added barchart with activityData as data source
    - Added activityDayNumbers const
    - Changed bar colors, size and border radius
    - Moved YAxis count to right
    - Moved Legend to top right
    - Removed vertical lines from CartesianGrid

--- src/components/ActivityChart.jsx
+++ src/components/ActivityChart.jsx
@@ -1,3 +1,14 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
 /**
  * ActivityChart component.
  *
@@ -6,10 +17,44 @@
  */
 export default function ActivityChart(props) {
   const activityData = props.data;
+  const activityDayNumbers = () => {
+    return activityData.sessions.map((session, index) => index + 1);
+  };
 
   return (
-    <div>
-      <p>{activityData.userId}</p>
-    </div>
+    <ResponsiveContainer width="90%" height="90%">
+      <BarChart data={activityData.sessions} barGap={12} barSize={8}>
+        <CartesianGrid strokeDasharray="3 3" vertical={false} />
+        <XAxis dataKey={activityDayNumbers} />
+        <YAxis yAxisId="kilogram" orientation="right" stroke="#282D30" />
+        <YAxis
+          yAxisId="calories"
+          orientation="left"
+          stroke="#E60000"
+          hide="true"
+        />
+        <Tooltip />
+        <Legend
+          layout="horizontal"
+          verticalAlign="top"
+          align="right"
+          wrapperStyle={{
+            paddingBottom: "2em",
+          }}
+        />
+        <Bar
+          yAxisId="kilogram"
+          dataKey="kilogram"
+          fill="#282D30"
+          radius={[20, 20, 0, 0]}
+        />
+        <Bar
+          yAxisId="calories"
+          dataKey="calories"
+          fill="#E60000"
+          radius={[20, 20, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
