f492b5f23ff5f76d27861f04a57595a8eec39913
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-07-03 15:58:29 +0200

    Made various changes :
    - Added renderTootlip func in ActivityChart to render custom tooltip
    - Added title in ActivityChart
    - Limited Cartesian grid count to 3 in ActivityChart
    - Added custom Legend in ActivityChart
    - Customized tick & ticklines in ActivityChart
    - Completed ActivityChart component
    - Adjusted .dashboard padding for better rendering in lower res

--- src/components/ActivityChart.jsx
+++ src/components/ActivityChart.jsx
@@ -17,40 +17,95 @@ import {
  */
 export default function ActivityChart(props) {
   const activityData = props.data;
+
   const activityDayNumbers = () => {
     return activityData.sessions.map((session, index) => index + 1);
   };
 
+  const renderTooltip = ({ active, payload }) => {
+    if (active && payload.length) {
+      return (
+        <div
+          style={{
+            background: "#E60000",
+            color: "#FFFFFF",
+            padding: "0.25em 0.5em",
+            textAlign: "center",
+            fontSize: "1rem",
+            fontWeight: "500",
+          }}
+        >
+          {payload.map((entry, index) => (
+            <p
+              key={index}
+              style={{ margin: "1.25em 0" }}
+            >{`${entry.value} ${entry.unit}`}</p>
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
-    <ResponsiveContainer width="90%" height="90%">
+    <ResponsiveContainer width="80%" height="80%">
       <BarChart data={activityData.sessions} barGap={12} barSize={8}>
-        <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey={activityDayNumbers} />
-        <YAxis yAxisId="kilogram" orientation="right" stroke="#282D30" />
+        <text x={0} y={20} fill="black" textAnchor="left">
+          <tspan style={{ fontWeight: 500 }}>Activité quotidienne</tspan>
+        </text>
+        <CartesianGrid strokeDasharray="3 3" opacity={0.5} vertical={false} />
+        <XAxis
+          dataKey={activityDayNumbers}
+          tickLine={false}
+          axisLine={{ stroke: "#DEDEDE" }}
+          tick={{ stroke: "#9B9EAC", fontWeight: "400" }}
+          dy={14}
+        />
+        <YAxis
+          yAxisId="kilogram"
+          orientation="right"
+          stroke="#282D30"
+          axisLine={false}
+          tickLine={false}
+          tickCount={3}
+          tick={{ stroke: "#9B9EAC", fontWeight: "400" }}
+          type="number"
+          domain={["dataMin - 5", "auto"]}
+          dx={14}
+        />
         <YAxis
           yAxisId="calories"
           orientation="left"
           stroke="#E60000"
-          hide="true"
+          axisLine={false}
+          tickLine={false}
+          tickCount={3}
+          hide={true}
+          domain={["dataMin - 50", "auto"]}
+          dx={-16}
         />
-        <Tooltip />
+        <Tooltip content={renderTooltip} />
         <Legend
           layout="horizontal"
           verticalAlign="top"
           align="right"
+          iconType="circle"
           wrapperStyle={{
-            paddingBottom: "2em",
+            paddingBottom: "4em",
           }}
         />
         <Bar
           yAxisId="kilogram"
+          name="Poids (kg)"
           dataKey="kilogram"
+          unit="kg"
           fill="#282D30"
           radius={[20, 20, 0, 0]}
         />
         <Bar
           yAxisId="calories"
+          name="Calories brûlées (kCal)"
           dataKey="calories"
+          unit="kCal"
           fill="#E60000"
           radius={[20, 20, 0, 0]}
         />
